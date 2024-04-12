70b7790bad15a172d092d3ad38309f1a3dbdb0d8
5_vania_5 <antonivan1234567@gmail.com>
2024-04-12 13:37:08 +0300

    async and await

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -1,115 +1,37 @@
 ﻿using System;
-using System.Data;
-using System.Globalization;
-using System.Text.Json;
-using System.Text.RegularExpressions;
-using AutoMapper;
-using Dapper;
-using HelloWorld.Data;
-using HelloWorld.Models;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
+
 
 namespace HelloWorld
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            IConfiguration configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json").Build();
-
-            DataContextDapper dapper = new DataContextDapper(configuration);
-
-
-
-            // File.WriteAllText("log.txt", sql);
-            // using StreamWriter openFile = new("log.txt", append: true);
-
-            // openFile.WriteLine(sql);
-
-            string computersJson = File.ReadAllText("ComputersSnake.json");
-
-            //this is one way to mapping data
-            // Mapper mapper = new Mapper(new MapperConfiguration((cfg) =>
-            // {
-            //     //simply copy from sourse.computer_id to destination.ComputerId,
-            //     //means copy from ComputerSnake to Computer
-            //     cfg.CreateMap<ComputerSnake, Computer>()
-            //     .ForMember(destination => destination.ComputerId, options =>
-            //     options.MapFrom(sourse => sourse.computer_id))
-            //     .ForMember(destination => destination.Motherboard, options =>
-            //     options.MapFrom(sourse => sourse.motherboard))
-            //     .ForMember(destination => destination.CPUCores, options =>
-            //     options.MapFrom(sourse => sourse.cpu_cores))
-            //     .ForMember(destination => destination.HasWifi, options =>
-            //     options.MapFrom(sourse => sourse.has_wifi))
-            //     .ForMember(destination => destination.HasLTE, options =>
-            //     options.MapFrom(sourse => sourse.has_lte))
-            //     .ForMember(destination => destination.ReleaseDate, options =>
-            //     options.MapFrom(sourse => sourse.release_date))
-            //     .ForMember(destination => destination.Price, options =>
-            //     options.MapFrom(sourse => sourse.price))
-            //     .ForMember(destination => destination.VideoCard, options =>
-            //     options.MapFrom(sourse => sourse.video_card));
-            // }));
-            //     //  System.Console.WriteLine(computersJson);
-            //     // JsonSerializerOptions options = new JsonSerializerOptions()
-            //     // {
-            //     //     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            //     // };
-            //in my Computer.cs i wrote JsonPropertyName like an another way to solve it
-            IEnumerable<Computer>? computersSystem = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJson);
-
-            if (computersSystem != null)
+            Task firstTask = new Task(() =>
             {
-                foreach (Computer computer in computersSystem)
-                {
-                    System.Console.WriteLine(computer.Motherboard);
-                }
-            }
-            //     {
-            //         foreach (Computer computer in computersNewtonSoft)
-            //         {
-            //             // System.Console.WriteLine(computer.Motherboard);
-            //             string sql = @"INSERT INTO TutorialAppSchema.Computer(
-            //         Motherboard,
-            //         HasWifi,
-            //         HasLTE,
-            //         ReleaseDate,
-            //         Price,
-            //         VideoCard
-            //         ) VALUES('" + EscapeSingleQuote(computer.Motherboard)
-            //            + "','" + computer.HasWifi
-            //            + "','" + computer.HasLTE
-            //            + "','" + computer.ReleaseDate
-            //            + "','" + computer.Price.ToString("0.00", CultureInfo.InvariantCulture)
-            //            + "','" + EscapeSingleQuote(computer.VideoCard)
-            //          + "')";
+                Thread.Sleep(100);
+                System.Console.WriteLine("Task 1");
+            });
+            firstTask.Start();
+            Task secondTask = ConsoleAfterDelayAsync("Task 2", 150);
 
-            //             dapper.ExecuteSql(sql);
-            //         }
-            //     }
+            ConsoleAfterDelay("Delay", 101);
+            Task ThirdTask = ConsoleAfterDelayAsync("Task 3", 50);
 
-            //     JsonSerializerSettings settings = new JsonSerializerSettings
-            //     {
-            //         ContractResolver = new CamelCasePropertyNamesContractResolver()
-            //     };
+            await firstTask;
+            System.Console.WriteLine("After the task was created");
 
-            //     string computersCopyNewtonsoft = JsonConvert.SerializeObject(computersNewtonSoft, settings);
-            //     File.WriteAllText("computersCopyNewtonsoft.txt", computersCopyNewtonsoft);
+        }
 
-            //     string computerCopySystem = System.Text.Json.JsonSerializer.Serialize(computersSystem);
-            //     File.WriteAllText("computersCopySystem.txt", computerCopySystem);
-            // }
-
-            // static string EscapeSingleQuote(string input)
-            // {
-            //     string output = input.Replace("'", "''");
-
-            //     return output;
+        static void ConsoleAfterDelay(string text, int delayTime)
+        {
+            Thread.Sleep(delayTime);
+            System.Console.WriteLine(text);
+        }
+        static async Task ConsoleAfterDelayAsync(string text, int delayTime)
+        {
+            await Task.Delay(delayTime);
+            System.Console.WriteLine(text);
         }
     }
 }
--- Hello World/obj/Debug/net8.0/Hello World.AssemblyInfo.cs
+++ Hello World/obj/Debug/net8.0/Hello World.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717bbd09ac8e7058a3cecafc063d5a2771192861")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8739b682bb6c537f89273570ee4a81947f351a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hello World")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
