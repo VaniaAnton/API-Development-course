cd78fe62a7dad52338445a66595c8705802a80e8
5_vania_5 <antonivan1234567@gmail.com>
2024-05-11 14:38:54 +0300

    MyPost endpoint

--- DotnetAPI/Controllers/PostController.cs
+++ DotnetAPI/Controllers/PostController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 namespace DotnetAPI.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
 
@@ -20,7 +20,7 @@ namespace DotnetAPI.Controllers
         }
 
         [HttpGet("Posts/{postId}/{userId}/{searchParam}")]
-        public IEnumerable<Post> GetPosts(int postId, int userId, string searchParam = "None")
+        public IEnumerable<Post> GetPosts(int postId = 0, int userId = 0, string searchParam = "None")
         {
             string sql = @" EXEC TutorialAppSchema.spPosts_Get";
             string parameters = "";
@@ -32,7 +32,7 @@ namespace DotnetAPI.Controllers
             {
                 parameters += ", @UserId=" + userId.ToString();
             }
-            if (searchParam != "None")
+            if (searchParam.ToLower() != "none")
             {
                 parameters += ", @SearchValue= '" + searchParam + "' ";
             }
@@ -47,14 +47,8 @@ namespace DotnetAPI.Controllers
         [HttpGet("MyPosts")]
         public IEnumerable<Post> GetMyPosts()
         {
-            string sql = @"SELECT [PostId],
-                    [UserId],
-                    [PostTitle],
-                    [PostContent],
-                    [PostCreated],
-                    [PostUpdated] 
-                FROM TutorialAppSchema.Posts 
-                    WHERE UserId = " + this.User.FindFirst("userId")?.Value;
+            string sql = @" EXEC TutorialAppSchema.spPosts_Get @UserId = " +
+                this.User.FindFirst("userId")?.Value;
 
             return _dapper.LoadData<Post>(sql);
         }
