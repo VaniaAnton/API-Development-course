86145cc64641deda0fc12a5dc1a7a9f854f11a20
5_vania_5 <antonivan1234567@gmail.com>
2024-05-21 01:12:36 +0300

    fixed

--- DotnetAPI/Controllers/UserCompleteController.cs
+++ DotnetAPI/Controllers/UserCompleteController.cs
@@ -1,10 +1,9 @@
+using System.Data;
 using Dapper;
 using DotnetAPI.Data;
 using DotnetAPI.DTOs;
 using DotnetAPI.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Data;
 
 namespace DotnetAPI.Controllers;
 
@@ -13,34 +12,40 @@ namespace DotnetAPI.Controllers;
 public class UserCompleteController : ControllerBase
 {
     DataContextDapper _dapper;
-    public UserCompleteController(IConfiguration configuration)
+    public UserCompleteController(IConfiguration config)
     {
-        _dapper = new DataContextDapper(configuration);
+        _dapper = new DataContextDapper(config);
     }
 
-    //HTTP endpoinds for User model
-    [HttpGet("GetUsers/{userId}/{Active}")]
-    // public IActionResult Test()
-    public IEnumerable<UserComplete> GetUsers(int userId, bool active)
+    [HttpGet("TestConnection")]
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
+
+    [HttpGet("GetUsers/{userId}/{isActive}")]
+    public IEnumerable<UserComplete> GetUsers(int userId, bool isActive)
     {
         string sql = @"EXEC TutorialAppSchema.spUsers_Get";
         string stringParameters = "";
         DynamicParameters sqlParameters = new DynamicParameters();
+
         if (userId != 0)
         {
-            stringParameters += ", @UserId = @UserIdParameter ";
+            stringParameters += ", @UserId=@UserIdParameter";
             sqlParameters.Add("@UserIdParameter", userId, DbType.Int32);
         }
-        if (active)
+        if (isActive)
         {
-            stringParameters += ", @Active = @ActiveParameter ";
-            sqlParameters.Add("@ActiveParameter", active, DbType.Boolean);
+            stringParameters += ", @Active=@ActiveParameter";
+            sqlParameters.Add("@ActiveParameter", isActive, DbType.Boolean);
         }
+
         if (stringParameters.Length > 0)
         {
-            sql += stringParameters.Substring(1);
+            sql += stringParameters.Substring(1);//, parameters.Length);
         }
-        System.Console.WriteLine(sql);
+
         IEnumerable<UserComplete> users = _dapper.LoadDataWithParameters<UserComplete>(sql, sqlParameters);
         return users;
     }
@@ -48,19 +53,19 @@ public class UserCompleteController : ControllerBase
     [HttpPut("UpsertUser")]
     public IActionResult UpsertUser(UserComplete user)
     {
-        string sql = @" EXEC TutorialAppSchema.spUser_Upsert
-            @FirstName = @FirstNameParameter,
-            @LastName = @LastNameParameter,
-            @Email = @EmailParameter,
-            @Gender = @GenderParameter,
-            @Active = @ActiveParameter,
-            @JobTitle = @JobTitleParameter,
-            @Department = @DepartmentParameter,
-            @Salary = @SalaryParameter,
+        string sql = @"EXEC TutorialAppSchema.spUser_Upsert
+            @FirstName = @FirstNameParameter, 
+            @LastName = @LastNameParameter, 
+            @Email = @EmailParameter, 
+            @Gender = @GenderParameter, 
+            @Active = @ActiveParameter, 
+            @JobTitle = @JobTitleParameter, 
+            @Department = @DepartmentParameter, 
+            @Salary = @SalaryParameter, 
             @UserId = @UserIdParameter";
 
-
         DynamicParameters sqlParameters = new DynamicParameters();
+
         sqlParameters.Add("@FirstNameParameter", user.FirstName, DbType.String);
         sqlParameters.Add("@LastNameParameter", user.LastName, DbType.String);
         sqlParameters.Add("@EmailParameter", user.Email, DbType.String);
@@ -75,16 +80,15 @@ public class UserCompleteController : ControllerBase
         {
             return Ok();
         }
+
         throw new Exception("Failed to Update User");
     }
-
-
 
     [HttpDelete("DeleteUser/{userId}")]
     public IActionResult DeleteUser(int userId)
     {
-        string sql = @" EXEC TutorialAppSchema.spUser_Delete 
-                @UserId = @UserIdParameter";
+        string sql = @"TutorialAppSchema.spUser_Delete
+            @UserId = @UserIdParameter";
 
         DynamicParameters sqlParameters = new DynamicParameters();
         sqlParameters.Add("@UserIdParameter", userId, DbType.Int32);
@@ -93,6 +97,7 @@ public class UserCompleteController : ControllerBase
         {
             return Ok();
         }
+
         throw new Exception("Failed to Delete User");
     }
 }
